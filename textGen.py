cc9f2fa09dc1a7901f8854e1daa13f728a8071eb
Mario Passlack <Mario.Passlack@Daa.de>
2025-09-25 13:03:57 +0200

    variablen program

---
+++ textGen.py
@@ -0,0 +1,9 @@
+# start
+name = "Ali"
+country = "Iran"
+age = "48"
+result = None
+
+result = "Hello my Name is "+name+ " and I am " +age+ " years old and I come from "+country
+print (result)
+# end
