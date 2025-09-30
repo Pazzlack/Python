39f146cba57b38073d498fde5444a23fb0600539
Mario Passlack <Mario.Passlack@Daa.de>
2025-09-30 14:34:54 +0200

    energieverbrauch.py

---
+++ Energieverbrauch.py
@@ -0,0 +1,16 @@
+Fernseher=3
+Fernseherhr=3
+Fernsehertage=365
+Herd=2
+Herdhr=2
+herdtage=208
+office=2
+officehr=4
+officetage=365
+Heizung=8
+Heizunghr=20
+Heizungtage=170
+Preis= 0.30
+euro=round(Fernseher*Fernseherhr*Fernsehertage+Herd*Herdhr*herdtage+office*officehr*officetage+Heizung*Heizunghr*Heizungtage*Preis,2)
+result=f"Der Preis beträgt: {euro} Euro" 
+print(result)
