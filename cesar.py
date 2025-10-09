5fc0655a7fd4ca867b8e6a405c563c005820cdf2
Mario Passlack <Mario.Passlack@Daa.de>
2025-10-09 13:12:58 +0200

    encrypt function

--- cesar.py
+++ cesar.py
@@ -1,10 +1,33 @@
 #implementation Cesar Encryption
 
-#this function do encryption
-def encrypt()->str:
-    klar_text = input ("Enter your text to encrypt: ")
+
+ALPHABET = [
+   ' ' 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+ALPHABET_LEN = len(ALPHABET)
+#this function gets the key from user and convert it to integer
+def getKey()->int:
+    #because always what user typed in cli will be saved as string
     key = input("Please enter enycryption key: ")
-    return "it is encrypted from " + klar_text + " with key " + key
+    #we must change the type to integer
+    key = int(key)
+    return key
+
+#this function encrypts the klar_text with the key
+def encrypt()->str:
+    result = ""
+    #klar_text is what user typed for example jojo
+    klar_text = input ("Enter your text to encrypt: ")
+    key = getKey()
+    for charecter in klar_text:
+        #we find position of char in alphabet
+        #when character == j ==> aplhabet_index = 9
+        alphabet_index = ALPHABET.index(charecter)
+        encryptedChar_index = (alphabet_index+key) % ALPHABET_LEN
+        result += ALPHABET[encryptedChar_index]
+    return result
 
 def decode()->str:
     cypher_text = input("\n please enter Text to be DECRYPTED: ")
